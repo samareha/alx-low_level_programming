072ebff4e4e131b3a1f4827f22ee00c8f9648d93
samareha <samar.ehab94@gmail.com>
2023-07-13 12:37:29 -0700

    m

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,7 @@
  * main - Entry point
  * Discription: A C programme that uses
  * Return: Always 0 (Success)
- */
+*/
 int main(void)
 {
 puts("/"Programming is like building a multilingual puzzle"):
