eb2877bb02868db594cea8df0bb808bced0e4d84
samareha <samar.ehab94@gmail.com>
2023-07-13 10:30:50 -0700

    m

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Discription : A C programme that prints with put function
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+   puts("/"Programming is like building a multilingual puzzle"):
+   return (0);
+}
