f77fd1cbfb5c31146346d91e5b214bfac8cec657
samareha <samar.ehab94@gmail.com>
2023-07-15 11:51:18 -0700

    My first c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,15 @@
 
 /**
  * main - Entry point
- * Discription: A C programme that uses
- * Return: Always 0 (Success)
+ *
+ * Description: A C program that prints with put function
+ *
+ * Return: Alawys 0 (Success)
 */
+
 int main(void)
 {
-puts("/"Programming is like building a multilingual puzzle"):
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
+
