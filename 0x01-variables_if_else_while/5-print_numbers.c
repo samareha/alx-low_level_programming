8be70e058cd923a3b785455d514c7f25f977ff5b
samareha <samar.ehab94@gmail.com>
2023-07-17 13:41:48 -0700

    a

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,23 +2,18 @@
 /**
   *main - prints all single digits
   *Return: 0
- */
+*/
 int main(void)
 {
-    int num;
+	int num;
+	num = 0;
+       	while (num <= 9)
 
-    num = 0;
+       	{
+	       	putchar("%i", num);
+		num++;
+	}
 
-    while (num <= 9)
-    {
-        putchar(num + '0');
-        if (num < 9)
-        {
-            putchar(',');
-            putchar(' ');
-        }
-        num++;
-    }
-    putchar('\n');
-    return (0);
+	putchar('\n');
+	return (0);
 }
---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print 0 - 9 using putchar
+ *              while using int variable
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 0)
+	{
+		/*convert digit to ASCII representation*/
+		putchar(digit + '0');
+		digit++;
+	}
+	putchar('\n');
+
+	return (0);
+}
