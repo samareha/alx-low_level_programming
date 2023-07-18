1c716e40f9e4a76a69ad8e996f95b4db7f9d9d40
samareha <samar.ehab94@gmail.com>
2023-07-18 10:53:15 -0700

    s

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,17 @@
+#include"main.h"
 
+/**
+ * main - entry point
+ *
+ * return: always 0 (success)
+*/
+
+void print_alphabet(void);
+{
+
+int ch;
+
+for (ch = 'a'; ch <= 'z'; ch++)
+_putchar(ch);
+_putchar('\n');
+}
