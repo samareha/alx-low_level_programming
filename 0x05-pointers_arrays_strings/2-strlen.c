9f4d4fbb702abb12a98e912d94bced8633c91746
samareha <samar.ehab94@gmail.com>
2023-07-24 11:21:58 -0700

    s

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strlen - check the code
+ * @s: string
+ * Return: Always 0.
+*/
+int _strlen(char *s)
+{
+	int lon = 0;
+
+	while (*S != '\0')
+	{
+		lon++;
+		*S++;
+	}
+	return (lon);
+}
